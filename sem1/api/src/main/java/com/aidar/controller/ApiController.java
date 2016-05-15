0370696ee389c4b1a4e37a4b00165debd23946f4
Shayfutdinov Aidar <exoooy@gmail.com>
2016-05-15 16:45:58 +0300

    sem2. separate properties

--- sem1/api/src/main/java/com/aidar/controller/ApiController.java
+++ sem1/api/src/main/java/com/aidar/controller/ApiController.java
@@ -1,5 +1,8 @@
 package com.aidar.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by paradise on 15.05.16.
  */
 @RestController
+@PropertySource("classpath:api.properties")
 public class ApiController {
+
+    @Autowired
+    private Environment environment;
 
     @RequestMapping("/api")
     public String api() {
-        return "Hello, world!";
+        return environment.getProperty("tokenSecret");
     }
 
 }
